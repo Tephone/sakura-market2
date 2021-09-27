e60318fd4a20fb284b53028f50e33b09d48bc388
naoko kasahara <kassa0918@gmail.com>
2021-09-27 17:47:22 +0900

    topページ作成、userにnameカラム追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admins, controllers: {sessions: 'admins/sessions'}
   devise_for :sellers, controllers: {sessions: 'sellers/sessions'}
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations', sessions: 'users/sessions'}
+  root 'tops#show'
+  resource :top, only:[:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
